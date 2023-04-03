7fb5c895fbac5b62f81ea77376b257fee05ba23a
DahyunKim <risehyun01@gmail.com>
2023-04-03 15:05:58 +0900

    CH1 풀이중

---
+++ ThisIsC++/CH1_EX_03.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+
+int main()
+{
+	std::string strData = "Test string";
+	std::cout << "Sample string" << std::endl;
+	std::cout << strData << std::endl;
+
+	strData = "New string";
+	std::cout << strData << std::endl;
+
+	std::cout << "저는 " << 20 << "살" << "입니다." << std::endl;
+
+	return 0;
+}
---
+++ ThisIsC++/CH1_EX_06.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+	// 인스턴스만 동적으로 생성하는 경우
+
+
+
+	return 0;
+}
