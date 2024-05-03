0de37f85e61619d01ec503e1210a415a3961471e
risehyun <risehyun01@gmail.com>
2024-05-03 23:09:07 +0900

    [ADD] LV11 PT 일부 풀이

---
+++ YC_STX/LV11_PT5.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int a = 0, b = 0;
+
+int GOP()
+{
+	cin >> a >> b;
+
+	return a * b;
+}
+
+int SUM()
+{
+	cin >> a >> b;
+
+	return a + b;
+}
+
+int main()
+{
+	int resultMul = GOP();
+	int resultSum = SUM();
+
+	if (resultMul > resultSum)
+	{
+		cout << "GOP>SUM";
+	}
+	else if (resultMul < resultSum)
+	{
+		cout << "GOP<SUM";
+	}
+	else
+	{
+		cout << "GOP==SUM";
+	}
+
+	return 0;
+}
