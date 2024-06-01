af03e40ef9cc7b64f1cbc5264f5e5a3ef1e35061
risehyun <risehyun01@gmail.com>
2024-06-01 23:52:27 +0900

    [ADD] LV04 MRV 풀이 완료

--- YC_STX/LV04_MRV_8.cpp
+++ YC_STX/LV04_MRV_8.cpp
@@ -2,38 +2,83 @@
 
 using namespace std;
 
-const double PI = 3.14;
-
-
 class Shape
 {
 private:
 	int type_;
 	double len_;
 
+	const double PI;
+	static int rectCount;
+	static int circleCount;
+
 public:
-
-	Shape() {};
-
-	Shape(int _type, int _len) 
-	: type_(_type), len_(_len){}
-
-	int GetArea() const
+	Shape(int _type = 1, int _len = 1) 
+	: type_(_type), len_(_len), PI(3.14)
 	{
-		return 0;
+		if (type_ == 1)
+		{
+			rectCount++;
+		}
+		else if (type_ == 2)
+		{
+			circleCount++;
+		}
+	}
+
+	~Shape() {
+		if (type_ == 1)
+		{
+			rectCount--;
+		}
+
+		else if (type_ == 2)
+		{
+			circleCount--;
+		}
+	}
+
+	double GetArea()
+	{
+		if (type_ == 1)
+		{
+			return (len_ * len_);
+		}
+		else if (type_ == 2)
+		{
+			return (PI * len_ * len_);
+		}
+	}
+
+	int GetRectCount()
+	{
+		return rectCount;
+	}
+
+	int GetCircleCount()
+	{
+		return circleCount;
 	}
 };
+
+int Shape::rectCount = 0;
+int Shape::circleCount = 0;
 
 int main()
 {
 	Shape shape1(1, 5);
-	Shape shape1(2, 5);
+	Shape shape2(2, 5);
 	Shape* p_rect = new Shape[3];
 
-	cout << "사각형 개수";
+	cout << "사각형 개수     : " << shape1.GetRectCount() << endl;
+	cout << "원 개수         : " << shape1.GetCircleCount() << endl;
+	cout << "shape2의 면적   : " << shape1.GetArea() << endl;
 
+	delete[] p_rect;
 
-
+	cout << "사각형 개수     : " << shape2.GetRectCount() << endl;
+	cout << "원 개수         : " << shape2.GetCircleCount() << endl;
+	cout << "shape2의 면적   : " << shape2.GetArea() << endl;
 
 	return 0;
 }
