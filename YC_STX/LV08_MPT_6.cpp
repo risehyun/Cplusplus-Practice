518bb2383cbdcce7b78b33d56718674e4d5ac53c
risehyun <risehyun01@gmail.com>
2024-06-18 09:49:36 +0900

    [ADD] LV08 MPT 풀이 완료

--- YC_STX/LV08_MPT_6.cpp
+++ YC_STX/LV08_MPT_6.cpp
@@ -5,32 +5,57 @@ using namespace std;
 int input[5] = {};
 int path[4] = {};
 
+
+int compare()
+{
+	int index = 0;
+
+	for (int i = 1; i < 4; i++)
+	{
+		index = i - 1;
+
+		int result = path[index] - path[i];
+
+		if (result < 0)
+		{
+			result *= -1;
+		}
+
+		cout << "**¾Õ : ÀÎµ¦½º - " << index << " - °ª : " << path[index] << " µÚ : ÀÎµ¦½º - " << i << " - °ª : " << path[i] << " ******* µÑÀÇ Â÷ : " << result << endl;
+
+		if (result > 3)
+		{
+			return 0;
+		}
+
+		index++;
+	}
+
+	return 1;
+
+}
+
 void run(int level, int* count)
 {
 	int flag = -1;
 
 	if (level == 4)
 	{
-		for (int i = 0; i < 3; i++)
-		{
-			int result = path[i+1] - path[i];
-
-			if(result > 3)
-			{
-				flag = 0;
-			}
-		}
+		flag = compare();
 
 		if (flag == 1)
 		{
+			(*count)++;
+
 			for (int i = 0; i < 4; i++)
 			{
 				cout << path[i];
 			}
 
-			cout << ": OK" << endl;
+			cout << ": OK" << endl << endl;
 
-			(*count)++;
+
+			return;
 		}
 		else
 		{
@@ -39,10 +64,11 @@ void run(int level, int* count)
 				cout << path[i];
 			}
 
-			cout << ": [NO]" << endl;
+			cout << ": [NO]" << endl << endl;
+			return;
 		}
 
-		return;
+
 	}
 
 	for (int i = 0; i < 5; i++)
@@ -53,6 +79,7 @@ void run(int level, int* count)
 	}
 
 }
+
 
 int main()
 {
@@ -65,7 +92,7 @@ int main()
 
 	run(0, &count);
 
-	cout << count;
+	cout << "Áßº¹¼ø¿­ÀÇ ÃÑ °¹¼ö : " << count;
 
 
 	return 0;
