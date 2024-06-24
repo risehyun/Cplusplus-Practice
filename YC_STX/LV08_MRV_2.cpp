e9ced86960de2624038b895e7cbee9a00c3f11e3
risehyun <risehyun01@gmail.com>
2024-06-24 22:22:08 +0900

    [ADD] LV08

--- YC_STX/LV08_MRV_2.cpp
+++ YC_STX/LV08_MRV_2.cpp
@@ -9,19 +9,44 @@ public:
 	int y;
 };
 
-int map[3][4] =
+int map[3][4] = {};
+
+int Check(int y, int x)
 {
+	int start = y + 1;
+	int end = 3 - y;
 
+	for (int i = start; i <= end; i++)
+	{
+		if (map[i][x] == 1)
+		{
+			return 1;
+		}
+	}
 
-};
+	start = x + 1;
+	end = 4 - x;
+
+	for (int i = start; i <= end; i++)
+	{
+		if (map[y][i] == 1)
+		{
+			return 1;
+		}
+	}
+
+	return 0;
+}
 
 int main()
 {
 	Point location[3] = {};
 
+	int result = 0;
+
 	for (int i = 0; i < 3; i++)
 	{
-		cin >> location[i].x >> location[i].y;
+		cin >> location[i].y >> location[i].x;
 	}
 
 	for (int z = 0; z < 3; z++)
@@ -38,7 +63,8 @@ int main()
 		}
 	}
 
-
+	cout << endl;
+	// 입력 결과 출력
 	for (int y = 0; y < 3; y++)
 	{
 		for (int x = 0; x < 4; x++)
@@ -48,14 +74,26 @@ int main()
 		cout << endl;
 	}
 
+	for (int y = 0; y < 3; y++)
+	{
+		for (int x = 0; x < 4; x++)
+		{
+			if (map[y][x] == 1)
+			{
+				result = Check(y, x);
 
-
-	//for (int i = 0; i < 3; i++)
-	//{
-	//	cout << location[i].x << "," << location[i].y << endl;
-	//}
-
-
+				if (result == 1)
+				{
+					cout << "위험" << endl;
+					break;
+				}
+				else
+				{
+					cout << "안전" << endl;
+				}
+			}
+		}
+	}
 
 	return 0;
 }
