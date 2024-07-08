ca9c50122c1e7859aa54da92dd6bbd6da52e50ae
risehyun <risehyun01@gmail.com>
2024-07-08 18:35:13 +0900

    [ADD] LV11

--- YC_STX/LV11_MHW_2.cpp
+++ YC_STX/LV11_MHW_2.cpp
@@ -7,6 +7,7 @@ struct Vector2
 	int x;
 	int y;
 
+	// 1
 	Vector2 operator+(Vector2 _other)
 	{
 		Vector2 newVector;
@@ -17,6 +18,7 @@ struct Vector2
 		return newVector;
 	}
 
+	// 2
 	Vector2 operator-(Vector2 _other)
 	{
 		Vector2 newVector;
@@ -27,5 +29,113 @@ struct Vector2
 		return newVector;
 	}
 
+	// 3
+	Vector2 operator*(Vector2 _other)
+	{
+		Vector2 newVector;
+
+		newVector.x = this->x * _other.x;
+		newVector.y = this->y * _other.y;
+
+		return newVector;
+	}
+
+	// 4
+	bool operator<(Vector2 _other)
+	{
+		return (this->x < _other.x && this->y < _other.y);
+	}
+
+	// 5
+	bool operator>(Vector2 _other)
+	{
+		return (this->x > _other.x && this->y < _other.y);
+	}
+
+	// 6
+	bool operator==(Vector2 _other)
+	{
+		return (this->x == _other.x && this->y == _other.y);
+	}
+
+	// 7
+	bool operator!=(Vector2 _other)
+	{
+		return (this->x != _other.x && this->y != _other.y);
+	}
+
+	// 8
+	Vector2 operator/(Vector2 _other)
+	{
+		Vector2 newVector;
+
+		newVector.x = this->x / _other.x;
+		newVector.y = this->y / _other.y;
+
+		return newVector;
+	}
 	
+	// 9
+	Vector2 operator%(Vector2 _other)
+	{
+		Vector2 newVector;
+
+		newVector.x = this->x % _other.x;
+		newVector.y = this->y % _other.y;
+
+		return newVector;
+	}
+
+	// 10
+	Vector2 operator++(int) 
+	{ 
+		Vector2 temp = *this;
+		this->x++;
+		this->y++;
+		return temp; 
+	}
+
 };
+
+int main()
+{
+	Vector2 test;
+	test.x = 3;
+	test.y = 3;
+
+	Vector2 test2;
+	test2.x = 1;
+	test2.y = 1;
+
+
+	Vector2 result;
+	result = test + test2;
+
+	cout << result.x << ", " << result.y << endl;
+
+	result = test - test2;
+	cout << result.x << ", " << result.y << endl;
+
+	result = test * test2;
+	cout << result.x << ", " << result.y << endl;
+
+	result = test / test2;
+	cout << result.x << ", " << result.y << endl;
+
+	result = test % test2;
+	cout << result.x << ", " << result.y << endl;
+
+	test++;
+	cout << test.x << ", " << test.y << endl;
+
+	bool resultBool = test > test2;
+	resultBool = test < test2;
+
+	resultBool = test > test2;
+
+	resultBool = test == test2;
+
+	resultBool = test != test2;
+
+	return 0;
+}
