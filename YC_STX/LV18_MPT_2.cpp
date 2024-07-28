de73333b7d17553e6dc519257ecb6731a8effe7c
risehyun <risehyun01@gmail.com>
2024-07-28 23:01:09 +0900

    [ADD] LV18

---
+++ YC_STX/LV18_MPT_2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int run(int level, int number)
+{
+
+	for (int i = 0; i < number; i++)
+	{
+
+	}
+}
+
+int main()
+{
+
+	int number = 0;
+
+	cin >> number;
+
+
+
+}
