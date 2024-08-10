f7b976df78a1533b8929834f3db5a39fbcebd08d
DahyunKim <risehyun01@gmail.com>
2024-08-10 22:44:29 +0900

    [ADD] STL 알고리즘

--- Charpter01 연산자 오버로딩/EX1_1.cpp
+++ Charpter01 연산자 오버로딩/EX1_1.cpp
@@ -2,8 +2,36 @@
 
 using namespace std;
 
-int main() 
+int main()
 {
+    int a = 0, b = 0, c = 0;
 
-	return 0;
+    cin >> a >> b >> c;
+
+    if (b + c > 59)
+    {
+        a += (b + c) / 60;
+
+        if (((b + c) % 60) == 0)
+        {
+            b = 0;
+        }
+        else
+        {
+            b = (b + c)%60;
+        }
+    }
+    else
+    {
+        b += c;
+    }
+
+    if (a > 23)
+    {
+        a -= 24;
+    }
+
+    cout << a << ' ' << b;
+
+    return 0;
 }
