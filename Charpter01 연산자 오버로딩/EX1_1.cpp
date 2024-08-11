bb6f81cfe75ef725d6151057b5a1ac6d701a6dfe
risehyun <risehyun01@gmail.com>
2024-08-11 23:46:40 +0900

    [ADD] STL 알고리즘

--- Charpter01 연산자 오버로딩/EX1_1.cpp
+++ Charpter01 연산자 오버로딩/EX1_1.cpp
@@ -1,37 +1,9 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-    int a = 0, b = 0, c = 0;
-
-    cin >> a >> b >> c;
-
-    if (b + c > 59)
-    {
-        a += (b + c) / 60;
-
-        if (((b + c) % 60) == 0)
-        {
-            b = 0;
-        }
-        else
-        {
-            b = (b + c)%60;
-        }
-    }
-    else
-    {
-        b += c;
-    }
-
-    if (a > 23)
-    {
-        a -= 24;
-    }
-
-    cout << a << ' ' << b;
-
-    return 0;
-}
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//
+//    return 0;
+//}
--- Charpter01 연산자 오버로딩/EX5_6.cpp
+++ Charpter01 연산자 오버로딩/EX5_6.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
 
+using namespace std;
+
+int main()
+{
+	vector<int> v;
+	
+	v.push_back(10);
+	v.push_back(20);
+	v.push_back(30);
+	v.push_back(40);
+	v.push_back(50);
+
+	list<int> lt;
+	lt.push_back(10);
+	lt.push_back(20);
+	lt.push_back(30);
+	lt.push_back(40);
+	lt.push_back(50);
+
+	sort(v.begin(), v.end());
+//	sort(lt.begin(), lt.end());	// 컴파일 에러 -> list는 임의 접근 반복자가 아닌
+								// 양방향 반복자를 사용하기 때문에 별도의 sort를 써야한다
+
+	return 0;
+}
