8230b433bb68e18855dde6d2b8b1a82d9aab0cd8
risehyun <risehyun01@gmail.com>
2024-08-14 21:31:57 +0900

    [ADD] STL iterator

--- Charpter01 연산자 오버로딩/EX1_1.cpp
+++ Charpter01 연산자 오버로딩/EX1_1.cpp
@@ -1,28 +1,11 @@
 //#include <iostream>
-//
+//#include <string>
 //using namespace std;
-//
 //int main()
 //{
-//    int n = 0, m = 0, i = 0, j = 0, k = 0;
-//    int bucket[101] = { 0 };
+//	string A = "", B = "";
 //
-//    cin >> n >> m;
+//	cin >> A >> B;
 //
-//    for (int index = 0; index < m; index++)
-//    {
-//        cin >> i >> j >> k;
-//
-//        for (int start = i; start <= j; start++)
-//        {
-//            bucket[start] = k;
-//        }
-//    }
-//
-//    for (int start = 1; start <= n; start++)
-//    {
-//        cout << bucket[start] << " ";
-//    }
-//
-//    return 0;
+//	return 0;
 //}
