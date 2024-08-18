5901edd8415f360a0cd493fe16775b572a32ef1d
risehyun <risehyun01@gmail.com>
2024-08-18 23:44:08 +0900

    [ADD] STL 기본 할당기

--- Charpter01 연산자 오버로딩/EX1_1.cpp
+++ Charpter01 연산자 오버로딩/EX1_1.cpp
@@ -1,11 +1,40 @@
-//#include <iostream>
-//#include <string>
-//using namespace std;
-//int main()
-//{
-//	string A = "", B = "";
-//
-//	cin >> A >> B;
-//
-//	return 0;
-//}
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int arr1[101][101] = { 0 };
+	int arr2[101][101] = { 0 };
+	int n = 0, m = 0;
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int k = 0; k < m; k++)
+		{
+			cin >> arr1[i][k];
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int k = 0; k < m; k++)
+		{
+			cin >> arr2[i][k];
+		}
+	}
+
+	for (int i = 0; i < n; i++) 
+	{
+		for (int k = 0; k < m; k++)
+		{
+			cout << arr1[i][k] + arr2[i][k] << ' ';
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
