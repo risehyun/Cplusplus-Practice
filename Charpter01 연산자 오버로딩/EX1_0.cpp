b610dd24381db8b028cce666bafc64915bfa2e2b
risehyun <risehyun01@gmail.com>
2024-08-25 20:31:42 +0900

    [ADD] set count

--- Charpter01 연산자 오버로딩/EX1_0.cpp
+++ Charpter01 연산자 오버로딩/EX1_0.cpp
@@ -3,6 +3,30 @@
 using namespace std;
 int main()
 {
+	int arr[9][9] = { 0 };
+	int maxValue = -1;
+	int maxY = -1, maxX = -1;
+
+	for (int y = 0; y < 9; y++)
+	{
+		for (int x = 0; x < 9; x++)
+		{
+			cin >> arr[y][x];
+
+			if (arr[y][x] > maxValue)
+			{
+				maxValue = arr[y][x];
+				maxY = y;
+				maxX = x;
+			}
+		}
+	}
+
+	maxY += 1;
+	maxX += 1;
+
+	cout << maxValue << '\n' << maxY << ' ' << maxX;
+
 
 
 	return 0;
