46d38ac585ff45a71822b2afcec3900b9b1f4f4e
risehyun <risehyun01@gmail.com>
2024-09-03 22:45:12 +0900

    [ADD]

--- Charpter01 연산자 오버로딩/EX1_0.cpp
+++ Charpter01 연산자 오버로딩/EX1_0.cpp
@@ -1,32 +1,44 @@
+// 선택정렬
+
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 int main()
 {
-	int n = 0;
 	vector<int> v;
+	int n = 0, index = 0, tmp = 0;
 
 	cin >> n;
 
-	// 입력받은 값을 한자리씩 끊어서 벡터에 하나씩 저장
-	while (n > 0)
+	for (int i = 0; i < n; i++)
 	{
-		v.push_back(n % 10);
-		n /= 10;
+		int input = 0;
+		cin >> input;
+
+		v.push_back(input);
 	}
 
-	// 오름차순으로 sort한 다음
-	sort(v.begin(), v.end());
-
-	// reverse 함수로 내부 데이터를 반대로 뒤집어줌
-	reverse(v.begin(), v.end());
-
-	// 최종 결과 출력
-	for (int i = 0; i < v.size(); i++) 
+	for (int i = 0; i < n - 1; i++)
 	{
-		cout << v[i];
+		int min = 1001;
+		for (int j = i; j < n; j++)
+		{
+			if (min > v[j])
+			{
+				min = v[j];
+				index = j;
+			}
+		}
+
+		tmp = v[i];
+		v[i] = v[index];
+		v[index] = tmp;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << v[i] << '\n';
 	}
 
 	return 0;
