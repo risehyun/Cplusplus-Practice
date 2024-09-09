a5cfe4db7285d53264cd1c290d74192aef05469d
risehyun <risehyun01@gmail.com>
2024-09-09 15:28:44 +0900

    [ADD]

--- Charpter01 연산자 오버로딩/EX1_0.cpp
+++ Charpter01 연산자 오버로딩/EX1_0.cpp
@@ -1,29 +1,36 @@
 #include <iostream>
-#include <stack>
-#include <queue>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int main()
+int main() 
 {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-	queue<int> Q;
-	int N;
-	cin >> N;
-	for (int i = 1; i <= N; i++) 
-	{
-		Q.push(i);
-	}
+    int n = 0, value = 0;
+    vector<int> v;
 
-	while (Q.size() != 1) {
-		Q.pop();
-		Q.push(Q.front());
-		Q.pop();
-	}
+    cin >> n;
 
-	cout << Q.front();
+    for (int i = 0; i < n; i++) 
+    {
+        cin >> value;
+        v.push_back(value);
+    }
 
-	return 0;
+    // 이진 탐색에 들어가기 전에 먼저 벡터 안의 데이터를 sort 해준다.
+    sort(v.begin(), v.end());
+ 
+    cin >> n;
+
+    // 이진 탐색 함수를 사용해 벡터 내부에 입력받은 값이 존재하는지 확인한다.
+    for (int i = 0; i < n; i++)
+    {
+        cin >> value;
+        cout << binary_search(v.begin(), v.end(), value) << ' ';
+    }
+
 }
