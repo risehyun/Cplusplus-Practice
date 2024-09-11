9e4b8fda6009cacd1aef7969af03f0b8ceb397cb
DahyunKim <risehyun01@gmail.com>
2024-09-11 23:35:54 +0900

    [ADD] 누적합

--- Charpter01 연산자 오버로딩/EX1_0.cpp
+++ Charpter01 연산자 오버로딩/EX1_0.cpp
@@ -1,39 +1,77 @@
+//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//
+//int main() 
+//{
+//	ios_base::sync_with_stdio(0);
+//	cin.tie(0); 
+//	cout.tie(0);
+//
+//	// 숫자의 개수와 누적합을 구할 횟수
+//	int n = 0, m = 0;
+//
+//	cin >> n >> m;
+//
+//	vector<int>input(n);
+//	vector<int>result(m);
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin >> input[i];
+//	}
+//
+//	for (int k = 0; k < m; k++)
+//	{
+//		int i = 0, j = 0, sum = 0;
+//
+//		cin >> i >> j;
+//
+//		for (int index = i; index <= j; index++)
+//		{
+//			sum += input[index-1];
+//		}
+//
+//		result[k] = sum;
+//	}
+//
+//	for (int i = 0; i < m; i++)
+//	{
+//		cout << result[i] << '\n';
+//	}
+//
+//	return 0;
+//}
+// 이렇게 풀면 시간 초과가 되기 때문에 누적합으로 풀어야 한다.
+
 #include <iostream>
-#include <algorithm>
-#include <vector>
 
 using namespace std;
+#define MAX 100001
 
-int main() 
-{
-	ios_base::sync_with_stdio(0);
-	cin.tie(0); 
-	cout.tie(0);
+int N, M;
+int arr[MAX];
 
-	int n = 0, m = 0;
-	cin >> n >> m;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
 
-	vector<string>input(n);
-	vector<string>result;
+    cin >> N >> M;
 
-	for (int i = 0; i < n; i++)
-	{
-		cin >> input[i];
-	}
+    //배열 입력 받기
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
 
-	sort(input.begin(), input.end());
+    //배열을 누적합로 변경
+    for (int i = 1; i <= N; i++)
+        arr[i] += arr[i - 1];
 
-	string target = "";
-	int count = 0;
-	for (int i = 0; i < m; i++) 
-	{
-		cin >> target;
+    //구간 입력 받고 누적 합 출력
+    for (int i = 0; i < M; i++) {
+        int x, y;
+        cin >> x >> y;
 
-		if (binary_search(input.begin(), input.end(), target))
-		{
-			count++;
-		}
-	}
-
-	cout << count << '\n';
+        cout << arr[y - 1] - arr[x - 2] << "\n";
+    }
 }
