9492bb4ae82b205668865aeea53784bbb0599a2e
risehyun <risehyun01@gmail.com>
2024-09-18 21:10:58 +0900

    [ADD]

--- Charpter01 연산자 오버로딩/EX1_0.cpp
+++ Charpter01 연산자 오버로딩/EX1_0.cpp
@@ -1,43 +1,10 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <map>
-
 using namespace std;
 
 int main() 
 {
-    int n = 0, m = 0, cnt = 0;
-    string s;
-    vector<string> result;
-    map<string, bool> list;
-
-    cin >> n >> m;
-
-    for (int i = 0; i < n; i++) 
-    {
-        cin >> s;
-        list.insert(make_pair(s, true));
-    }
-
-    for (int i = 0; i < m; i++) 
-    {
-        cin >> s;
-        if (list[s]) 
-        {
-            result.push_back(s);
-            cnt++;
-        }
-    }
-
-    cout << cnt << '\n';
-    sort(result.begin(), result.end());
-
-    for (int i = 0; i < result.size(); i++) 
-    {
-        cout << result[i] << '\n';
-    }
-
+    int n;
+    cin >> n;
+    cout << n << '\n' << 1;
     return 0;
 }
