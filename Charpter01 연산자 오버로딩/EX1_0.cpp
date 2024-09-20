871a7588e0c0c363b4f145803ce467c24ffab6fa
risehyun <risehyun01@gmail.com>
2024-09-20 16:10:31 +0900

    [ADD]

--- Charpter01 연산자 오버로딩/EX1_0.cpp
+++ Charpter01 연산자 오버로딩/EX1_0.cpp
@@ -1,45 +1,25 @@
 #include <iostream>
-#include <cctype>
-#include <string>
-#include <algorithm>
-#include <map>
 using namespace std;
 
-string pokemon[100000];
-
-int main() {
-	map<string, int> P;
-	int N;               // 도감에 수록된 포켓몬 수
-	int M;               // 맞춰야 하는 문제의 수
-	string input_name;   // 맞춰야 되는 문제 중 입력된 이름
-	int input_num;       // 맞춰야 되는 문제 중 입력된 번호
-
-	ios::sync_with_stdio(0);
+int main() 
+{
+	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 
-	cin >> N >> M;
+	int a = 0, b = 0, c = 0, d = 0, e = 0, f = 0;
 
-	for (int i = 0; i < N; i++) 
+	cin >> a >> b >> c >> d >> e >> f;
+
+	for (int x = -999; x <= 999; x++)
 	{
-		cin >> pokemon[i];
-		P.insert(make_pair(pokemon[i], i));
-	}
-
-	for (int i = 0; i < M; i++) 
-	{
-		cin >> input_name;
-
-		// 1. 입력된 것이 숫자인 경우
-		if (isdigit(input_name[0]) != 0) {
-			input_num = stoi(input_name) - 1;
-			cout << pokemon[input_num] << "\n";
-		}
-
-		// 2. 입력된 것이 문자인 경우
-		else {
-			auto index = P.find(input_name);
-			cout << index->second + 1 << "\n";
+		for (int y = -999; y <= 999; y++)
+		{
+			if ((a * x + b * y == c) && (d * x + e * y == f))
+			{
+				cout << x << ' ' << y;
+				break;
+			}
 		}
 	}
 
