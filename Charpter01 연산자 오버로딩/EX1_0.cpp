66950bad2c72ccd41b07f5270b070d300cf39794
risehyun <risehyun01@gmail.com>
2024-09-22 21:58:50 +0900

    [ADD]

--- Charpter01 연산자 오버로딩/EX1_0.cpp
+++ Charpter01 연산자 오버로딩/EX1_0.cpp
@@ -1,47 +1,10 @@
 #include <iostream>
-#include <stack>
-
 using namespace std;
-
-stack<int> s;
-int N = 0, T = 0, cnt = 1;
 
 int main() 
 {
-	ios_base::sync_with_stdio(0); 
-	cin.tie(0);
-	cout.tie(0);
-
-	cin >> N;
-
-	while (N--) 
-	{
-		cin >> T;
-
-		if (T == cnt)
-		{
-			cnt++;
-		}
-		else
-		{
-			s.push(T);
-		}
-
-		while (!s.empty() && s.top() == cnt) 
-		{
-			s.pop();
-			cnt++;
-		}
-	}
-
-	if (s.empty())
-	{
-		cout << "Nice";
-	}
-	else
-	{
-		cout << "Sad";
-	}
-
-	return 0;
+    long long n;
+    cin >> n;
+    cout << n * n << '\n' << 2;
+    return 0;
 }
