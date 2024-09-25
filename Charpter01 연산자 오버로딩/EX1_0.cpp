6e948effd8f72cf92b61dd85871084327558a34f
risehyun <risehyun01@gmail.com>
2024-09-25 20:12:11 +0900

    [ADD]

--- Charpter01 연산자 오버로딩/EX1_0.cpp
+++ Charpter01 연산자 오버로딩/EX1_0.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
+#include <map>
 using namespace std;
 
 int main() 
 {
-    long long n;
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    map<string, string, greater<>> m;
+    int n = 0;
+
     cin >> n;
-    cout << (n - 1) * n / 2 << '\n' << 2;
+
+    for (int i = 0; i < n; i++) 
+    {
+        string name, EorL;
+
+        cin >> name >> EorL;
+        m[name] = EorL;
+    }
+
+    map<string, string>::iterator it;
+
+    for (it = m.begin(); it != m.end(); it++) 
+    {
+        if (it->second == "enter") 
+        {
+            cout << it->first << "\n";
+        }
+    }
+
     return 0;
 }
