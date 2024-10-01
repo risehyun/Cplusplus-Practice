d65d877801836c06bc7e6cacdb81a66722723fa9
risehyun <risehyun01@gmail.com>
2024-10-01 21:46:19 +0900

    [ADD]

--- Charpter01 연산자 오버로딩/EX1_0.cpp
+++ Charpter01 연산자 오버로딩/EX1_0.cpp
@@ -3,9 +3,18 @@ using namespace std;
 
 int main() 
 {
-    long long n;
-    cin >> n;
-    cout << (n - 2) * (n - 1) * n / 6 << '\n' << 3;
+    int a1 = 0, a0 = 0, c = 0, n0 = 0;
+
+    cin >> a1 >> a0 >> c >> n0;
+
+    if (a1 * n0 + a0 <= c * n0 && a1 <= c)
+    {
+        cout << 1 << endl;
+    }
+    else
+    {
+        cout << 0 << endl;
+    }
 
     return 0;
 }
