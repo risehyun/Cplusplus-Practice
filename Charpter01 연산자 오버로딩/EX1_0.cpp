78e0eb64702379e5d2423b56d572cb47f8252f66
risehyun <risehyun01@gmail.com>
2024-10-12 19:09:12 +0900

    [ADD]

--- Charpter01 연산자 오버로딩/EX1_0.cpp
+++ Charpter01 연산자 오버로딩/EX1_0.cpp
@@ -1,10 +1,53 @@
 #include <iostream>
+#include <string>
+#include <set>
 using namespace std;
 
-int n;
+int num;
+set<string> ccDanceList;
+bool isChance;
+int section;
+string name1, name2;
 
-int main()
+int main() 
 {
-    cin >> n;
-    cout << (1 << n);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> num;
+
+    ccDanceList.insert("ChongChong");
+
+    for (int i = 0; i < num; i++) 
+    {
+        isChance = false;
+        cin >> name1 >> name2;
+
+        if (ccDanceList.find(name1) != ccDanceList.end()) 
+        {
+            isChance = !isChance;
+            section = 1;
+        }
+
+        if (ccDanceList.find(name2) != ccDanceList.end()) 
+        {
+            isChance = !isChance;
+            section = 2;
+        }
+
+        if (isChance) 
+        {
+            if (section == 1) 
+            {
+                ccDanceList.insert(name2);
+            }
+            else {
+                ccDanceList.insert(name1);
+            }
+        }
+    }
+
+    cout << ccDanceList.size();
+
 }
