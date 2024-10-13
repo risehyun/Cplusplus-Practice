3535af7e2f52a78c3c3218be7f719918f94b3ac1
risehyun <risehyun01@gmail.com>
2024-10-13 20:29:51 +0900

    [ADD]

--- Charpter01 연산자 오버로딩/EX1_0.cpp
+++ Charpter01 연산자 오버로딩/EX1_0.cpp
@@ -1,53 +1,38 @@
 #include <iostream>
-#include <string>
-#include <set>
 using namespace std;
 
-int num;
-set<string> ccDanceList;
-bool isChance;
-int section;
-string name1, name2;
+int arr[41] = { 0 };
+int n, cnt1, cnt2;
 
-int main() 
+int fib(int N)
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    cin >> num;
-
-    ccDanceList.insert("ChongChong");
-
-    for (int i = 0; i < num; i++) 
+    if (N == 1 || N == 2)
     {
-        isChance = false;
-        cin >> name1 >> name2;
+        cnt1++;
+        return 1;
+    }
+    else
+    {
+        return (fib(N - 1) + fib(N - 2));
+    }
+}
 
-        if (ccDanceList.find(name1) != ccDanceList.end()) 
-        {
-            isChance = !isChance;
-            section = 1;
-        }
+int main()
+{
 
-        if (ccDanceList.find(name2) != ccDanceList.end()) 
-        {
-            isChance = !isChance;
-            section = 2;
-        }
+    cin >> n;
 
-        if (isChance) 
-        {
-            if (section == 1) 
-            {
-                ccDanceList.insert(name2);
-            }
-            else {
-                ccDanceList.insert(name1);
-            }
-        }
+    fib(n);
+
+    arr[1] = arr[2] = 1;
+
+    for (int i = 3; i <= n; i++)
+    {
+        arr[i] = arr[i - 1] + arr[i - 2];
+        cnt2++;
     }
 
-    cout << ccDanceList.size();
+    cout << cnt1 << " " << cnt2;
 
+    return 0;
 }
