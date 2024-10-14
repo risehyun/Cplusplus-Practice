b67c9eeb28827af55e9657b89b0a507cf29c5b37
risehyun <risehyun01@gmail.com>
2024-10-14 23:15:23 +0900

    [ADD]

--- Charpter01 연산자 오버로딩/EX1_0.cpp
+++ Charpter01 연산자 오버로딩/EX1_0.cpp
@@ -1,38 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int arr[41] = { 0 };
-int n, cnt1, cnt2;
-
-int fib(int N)
+int fibo(int num)
 {
-    if (N == 1 || N == 2)
-    {
-        cnt1++;
+    if (num == 0)
+        return 0;
+    if (num == 1)
         return 1;
-    }
-    else
-    {
-        return (fib(N - 1) + fib(N - 2));
-    }
+    return fibo(num - 2) + fibo(num - 1);
 }
+
+int num;
 
 int main()
 {
-
-    cin >> n;
-
-    fib(n);
-
-    arr[1] = arr[2] = 1;
-
-    for (int i = 3; i <= n; i++)
-    {
-        arr[i] = arr[i - 1] + arr[i - 2];
-        cnt2++;
-    }
-
-    cout << cnt1 << " " << cnt2;
-
-    return 0;
+    cin >> num;
+    cout << fibo(num);
 }
