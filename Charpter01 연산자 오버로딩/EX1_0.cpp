20a6ab9d33f145f750b22717595e66b845196cb3
risehyun <risehyun01@gmail.com>
2024-10-18 23:40:56 +0900

    [ADD]

--- Charpter01 연산자 오버로딩/EX1_0.cpp
+++ Charpter01 연산자 오버로딩/EX1_0.cpp
@@ -1,31 +1,35 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <climits>
+
+int arr[9] = { 0, };
+bool visited[9] = { false, };
+
 using namespace std;
-vector <vector<int>> v;
-int n;
-int result = INT_MAX;
-
-int main() 
+void dfs(int num, int cnt, int n, int m)
 {
-
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-
-	cin >> n;
-
-	vector <vector <int>>dp(n + 1, vector<int>(3, 0));
-
-	for (int i = 1; i <= n; i++) 
+	if (cnt == m)
 	{
-		cin >> dp[i][0] >> dp[i][1] >> dp[i][2];
-		dp[i][0] += min(dp[i - 1][1], dp[i - 1][2]);
-		dp[i][1] += min(dp[i - 1][0], dp[i - 1][2]);
-		dp[i][2] += min(dp[i - 1][0], dp[i - 1][1]);
+		for (int i = 0; i < m; i++)
+			cout << arr[i] << ' ';
+
+		cout << '\n';
+		return;
 	}
 
-	cout << min(dp[n][0], min(dp[n][1], dp[n][2]));
+	for (int i = num; i <= n; i++)
+	{
+		if (!visited[i])
+		{
+			visited[i] = true;
+			arr[cnt] = i;
+			dfs(i + 1, cnt + 1, n, m);
+			visited[i] = false;
+		}
+	}
+}
 
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	dfs(1, 0, n, m);
 }
