7ce3aa74a2435a42994533e8c2fadb7945b97444
risehyun <risehyun01@gmail.com>
2024-10-22 22:22:00 +0900

    [ADD]

--- Charpter01 연산자 오버로딩/EX1_0.cpp
+++ Charpter01 연산자 오버로딩/EX1_0.cpp
@@ -1,50 +1,15 @@
 #include <iostream>
-#include <vector>
-# define MAX 8
-
 using namespace std;
 
-int arr[MAX];
-bool visited[MAX];
-vector<int> vec;
-int cnt, n, m;
-
-void Print() 
+int main() 
 {
-    for (int i = 0; i < vec.size(); i++)
-        cout << vec[i] << " ";
-    cout << '\n';
-    return;
-}
+    int num;
+    cin >> num;
 
-void DFS(int cnt) 
-{
-    if (cnt == m) 
+    int result = 0;
+    for (int i = 1; i * i <= num; i++) 
     {
-        Print();
-        return;
+        result++;
     }
-
-    for (int i = 0; i < n; i++) 
-    {
-        if (visited[i]) continue;
-        visited[i] = true;
-        vec.push_back(arr[i]);
-        DFS(cnt + 1);
-        vec.pop_back();
-        visited[i] = false;
-    }
-}
-
-int main(void) 
-{
-    cin >> n >> m;
-
-    for (int i = 0; i < n; i++) 
-    {
-        arr[i] = i + 1;
-        visited[i] = false;
-    }
-
-    DFS(0);
+    cout << result;
 }
