04289ddd625c7d41223187c733ad0ff817351c66
DahyunKim <risehyun01@gmail.com>
2024-11-08 23:30:42 +0900

    [ADD]

--- Charpter01 연산자 오버로딩/EX1_0.cpp
+++ Charpter01 연산자 오버로딩/EX1_0.cpp
@@ -1,22 +1,40 @@
 #include <iostream>
+#include <stack>
+#include <string>
+
 using namespace std;
+
+typedef pair<int, int> pairs;
+deque<pairs> balloon_deque;
 
 int main() 
 {
-    int n = 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, value;
     cin >> n;
 
-    for (int y = 1; y <= n; y++)
-    {
-        for (int x = 1; x <= n - y; x++)
-        {
-            cout << ' ';
-        }
-        for (int x = 1; x <= 2 * y - 1; x++)
-        {
-            cout << '*';
+    for (int i = 0; i < n; i++) {
+        cin >> value;
+        balloon_deque.emplace_back(i + 1, value);
+    }
+
+    while (!balloon_deque.empty()) {
+        pairs balloon = balloon_deque.front();
+        cout << balloon.first << ' ';
+        balloon_deque.pop_front();
+
+        for (int i = 1; i < balloon.second; i++) {
+            balloon_deque.push_back(balloon_deque.front());
+            balloon_deque.pop_front();
         }
 
-        cout << '\n';
+        for (int i = 0; i > balloon.second; i--) {
+            balloon_deque.push_front(balloon_deque.back());
+            balloon_deque.pop_back();
+        }
     }
+
+    return 0;
 }
